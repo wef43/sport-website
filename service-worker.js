a40f8a1d7a3246d85fd58f31103994bc40740d0c
wef43 <mrgoulik2@gmail.com>
2025-07-17 15:36:15 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,27 @@
-const CACHE_NAME = 'sport-site-v2';
-const urlsToCache = [
+const staticCacheName = 'sport-site-v1';
+const assetUrls = [
   '/',
   '/index.html',
   '/football.html',
   '/basketball.html',
   '/styles.css',
   '/images/football.jpg',
-  '/images/basketball.jpg'
+  '/images/basketball.jpg',
+  '/images/icon-192x192.png',
+  '/images/icon-512x512.png',
+  '/manifest.json'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', async event => {
+  const cache = await caches.open(staticCacheName);
+  await cache.addAll(assetUrls);
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
-  );
+self.addEventListener('fetch', event => {
+  event.respondWith(cacheFirst(event.request));
 });
+
+async function cacheFirst(request) {
+  const cached = await caches.match(request);
+  return cached || await fetch(request);
+}
