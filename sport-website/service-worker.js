26f02ce8415e05afb4679ebb18709792e59c5536
wef43 <mrgoulik2@gmail.com>
2025-07-17 14:51:12 +0300

    Add files via upload

---
+++ sport-website/service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'sport-site-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/football.html',
+  '/basketball.html',
+  '/styles.css',
+  '/images/football.jpg',
+  '/images/basketball.jpg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
